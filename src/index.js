22bfabcb32eea434d1f09035e21e9a1e292b7c88
MARCOS ALBERTO QUEIROZ <marcosalbertoqueiroz@MacBook-Pro-de-MARCOS.local>
2020-07-31 00:07:26 -0300

    upate

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/home/App';
+import CadastroVideo from './pages/cadastro/video';
+import {BrowserRouter,Switch,Route} from "react-router-dom";
+import CadastroCategoria from './pages/cadastro/categoria';
+
+/*
+function CadastroVideo() {
+  return (
+    <div>
+      Pagina Cadastro de Video
+    </div>
+  );
+}
+*/
+
+const Pagigina404 = () => (
+  <div>Pagina nao encontrada: 404 - Not Found</div>
+) 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+    <Switch>
+      <Route path = "/" component = {Home} exact /> 
+      <Route path = "/cadastro/video" component = {CadastroVideo} /> 
+      <Route path = "/cadastro/categoria" component = {CadastroCategoria} />       
+      <Route component = {Pagigina404} exact /> 
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
