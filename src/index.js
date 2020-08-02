1d7e067b13008b7a049df7b8e4561f6c49e4b743
MARCOS ALBERTO QUEIROZ <marcosalbertoqueiroz@MacBook-Pro-de-MARCOS.local>
2020-08-01 22:44:43 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/home/App';
 import CadastroVideo from './pages/cadastro/video';
-import {BrowserRouter,Switch,Route} from "react-router-dom";
 import CadastroCategoria from './pages/cadastro/categoria';
 
-/*
-function CadastroVideo() {
-  return (
-    <div>
-      Pagina Cadastro de Video
-    </div>
-  );
-}
-*/
-
-const Pagigina404 = () => (
-  <div>Pagina nao encontrada: 404 - Not Found</div>
-) 
+// Desafio master blaster na descrição
+// Colocar um jogo nessa página: https://www.youtube.com/watch?v=jOAU81jdi-c :)
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-
   <BrowserRouter>
     <Switch>
-      <Route path = "/" component = {Home} exact /> 
-      <Route path = "/cadastro/video" component = {CadastroVideo} /> 
-      <Route path = "/cadastro/categoria" component = {CadastroCategoria} />       
-      <Route component = {Pagigina404} exact /> 
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
 );
-
